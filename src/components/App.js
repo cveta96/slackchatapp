b0afc8e4a6ddd0ccc15a7f169a7402fe9b426283
cveta96 <djnik8225@gmail.com>
2019-01-23 18:01:20 +0100

    UserPanel under construction

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
+import { Grid, GridColumn } from "semantic-ui-react";
+import { connect } from "react-redux";
 
-class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+import ColorPanel from "./colorpanel/ColorPanel";
+import SidePanel from "./sidepanel/SidePanel";
+import Messages from "./messages/Messages";
+import MetaPanel from "./metapanel/MetaPanel";
 
-export default App;
+const App = ({ currentUser }) => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel currentUser={currentUser} />
+    <GridColumn style={{ marginLeft: 320 }}>
+      <Messages />
+    </GridColumn>
+    <GridColumn width={4}>
+      <MetaPanel />
+    </GridColumn>
+  </Grid>
+);
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/sidepanel/UserPanel.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import {
+  Grid,
+  GridColumn,
+  GridRow,
+  Header,
+  HeaderContent,
+  Icon,
+  Dropdown,
+  Image
+} from "semantic-ui-react";
+import firebase from "../../firebase";
+
+class UserPanel extends Component {
+  state = {
+    user: this.props.currentUser
+  };
+
+  dropdownOptions = () => [
+    {
+      key: "user",
+      text: (
+        <span>
+          Signed in as <strong>{this.state.user.displayName}</strong>
+        </span>
+      ),
+      disabled: true
+    },
+    {
+      key: "avatar",
+      text: <span>Change Avatar</span>
+    },
+    {
+      key: "signout",
+      text: <span onClick={this.handleSignOut}>Sign Out</span>
+    }
+  ];
+
+  handleSignOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => console.log("Signed out!"));
+  };
+
+  render() {
+    const { user } = this.state;
+    return (
+      <Grid style={{ background: "4c3c4c" }}>
+        <GridColumn>
+          <GridRow style={{ padding: "1.2em", margin: 0 }}>
+            {/* App Header */}
+            <Header inverted floated="left" as="h2">
+              <Icon name="code" />
+              <HeaderContent>DevChat</HeaderContent>
+            </Header>
+            {/* User DropDown */}
+            <Header style={{ padding: "0.25em" }} as="h4" inverted>
+              <Dropdown
+                trigger={
+                  <span>
+                    <Image src={user.photoURL} spaced="right" avatar />
+                    {user.displayName}
+                  </span>
+                }
+                options={this.dropdownOptions()}
+              />
+            </Header>
+          </GridRow>
+        </GridColumn>
+      </Grid>
+    );
+  }
+}
+
+export default UserPanel;
