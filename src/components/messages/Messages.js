0988f4d0490cbcd113e2dd8b9b16b5b4bcf0808e
cveta96 <djnik8225@gmail.com>
2019-01-24 23:03:58 +0100

    Almost finished messages panel, need to add componentwillunmount to remove listener for messages

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,8 +1,71 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import MessagesHeader from "./MessagesHeader";
+import MessagesForm from "./MessagesForm";
+import firebase from "../../firebase";
+import Message from "./Message";
 
 class Messages extends Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+    messages: [],
+    messagesLoading: true
+  };
+
+  componentDidMount() {
+    const { channel, user } = this.state;
+    if (channel && user) {
+      this.addListeners(channel.id);
+    }
+  }
+
+  addListeners = channelId => {
+    this.addMessageListener(channelId);
+  };
+
+  addMessageListener = channelId => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on("child_added", snap => {
+      loadedMessages.push(snap.val());
+      console.log(loadedMessages);
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false
+      });
+    });
+  };
+
+  displayMessages = messages =>
+    messages.length > 0 &&
+    messages.map(message => (
+      <Message
+        key={message.timestamp}
+        message={message}
+        user={this.state.user}
+      />
+    ));
+
   render() {
-    return <div>Messages</div>;
+    const { messagesRef, channel, user, messages } = this.state;
+    return (
+      <Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">
+            {this.displayMessages(messages)}
+          </Comment.Group>
+        </Segment>
+
+        <MessagesForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
+      </Fragment>
+    );
   }
 }
 
